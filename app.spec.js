0d18765dea943ccccee1d9499a3d832750645e9d
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-11-07 00:53:55 +0330

    Test added, run the test and GO!
    you can use the test and comments, to do the task!

--- app.spec.js
+++ app.spec.js
@@ -1,2 +1,10 @@
+import { describe, expect, it } from "vitest";
+import * as functions from "./app";
 
-
+describe("At every function call, it must calculate the date of measurement", () => {
+  const d = new Date();
+  it("When the function called must return the current time", () => {
+    const result = functions.dateOfMeasurement();
+    expect(result).toEqual(d);
+  });
+});
