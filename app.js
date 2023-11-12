5b193d8ce0164a15e409f6166ef812e422832752
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-11-12 15:29:12 +0330

    render method refactored successfully

--- app.js
+++ app.js
@@ -1,59 +1,36 @@
-// history = {
-//   durationToReach: "8 Day(s)",
-//   reach: 69000,
-//   lastHpd: 100,
-//   currentHpd: 125,
-//   lastHph: 4.16,
-//   currentHph: 5.2,
-//   averageHpd: 112.5,
-//   averageHph: 4.7,
-//   dateOfMeasurement:
-//     "Wed Oct 11 2023 20:27:18 GMT+0330 (Iran Standard Time)", <<<<<<<<<<<<
-// }
+const history = {
+  2023: {
+    11: [
+      {
+        durationToReach: "7 Day(s)",
+        reach: 72000,
+        lastHpd: "",
+        currentHpd: "",
+        lastHph: "",
+        currentHph: "",
+        averageHpd: "",
+        averageHph: "",
+        dateOfMeasurement:
+          "Sun Nov 05 2023 14:55:38 GMT+0330 (Iran Standard Time)",
+      },
+    ],
+  },
+};
+
+const html = {
+  dateOfMeasurement: document.getElementById("dateOfMeasurement"),
+};
 
 class Report1000h {
-  constructor() {
-    this.dateOfMeasurement = document.getElementById('dateOfMeasurement');
-    this.history = {
-      2023: {
-        11: [
-          {
-            durationToReach: '7 Day(s)',
-            reach: 72000,
-            lastHpd: '',
-            currentHpd: '',
-            lastHph: '',
-            currentHph: '',
-            averageHpd: '',
-            averageHph: '',
-            dateOfMeasurement:
-              'Sun Nov 05 2023 14:55:38 GMT+0330 (Iran Standard Time)'
-          }
-        ]
-      }
-    };
-  }
+  constructor() {}
 
-  getTime() {
-    return new Date();
-  }
-
-  //implement render method
   render() {
-    this.dateOfMeasurement.innerText = `${this.getTime()}`;
+    html.dateOfMeasurement.innerText = `${new Date()}`;
   }
 }
 
-const report1000h = new Report1000h(dateOfMeasurement);
-report1000h.render();
-// const d = new Date();
-// const dateOfMeasurement = () => {
-//   return d;
-// };
+const report11122023 = new Report1000h();
+report11122023.render();
 
-//render
-// document.getElementById(
-//   "dateOfMeasurement"
-// ).innerText = `${dateOfMeasurement()}`;
-
+//This export is for testing purposes
 // export { dateOfMeasurement };
