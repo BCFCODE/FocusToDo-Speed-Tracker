ce931b711624131d2dbf396102f7f68032332e1e
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-11-12 12:48:34 +0330

    class Report1000h and render method added

--- app.js
+++ app.js
@@ -11,11 +11,38 @@
 //     "Wed Oct 11 2023 20:27:18 GMT+0330 (Iran Standard Time)", <<<<<<<<<<<<
 // }
 
+class Report1000h {
+  constructor() {
+    this.history = {
+      2023: {
+        11: [
+          {
+            durationToReach: "7 Day(s)",
+            reach: 72000,
+            lastHpd: "",
+            currentHpd: "",
+            lastHph: "",
+            currentHph: "",
+            averageHpd: "",
+            averageHph: "",
+            dateOfMeasurement:
+              "Sun Nov 05 2023 14:55:38 GMT+0330 (Iran Standard Time)",
+          },
+        ],
+      },
+    };
+  }
+
+  
+  render() {}
+}
+
 const d = new Date();
 const dateOfMeasurement = () => {
   return d;
 };
 
+//render
 document.getElementById(
   "dateOfMeasurement"
 ).innerText = `${dateOfMeasurement()}`;
