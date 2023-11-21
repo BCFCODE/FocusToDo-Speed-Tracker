75d70dd8c6fd0712981a800f9d1f17b1fb60db2d
Majid <majidbabak@gmail.com>
2023-11-21 15:57:28 +0330

    calculate averageHourPerDay

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-import history from "./src/database.js";
+import history from './src/database.js';
 
 // const html = {
 //   dateOfMeasurement: document.getElementById("dateOfMeasurement"),
 // };
 
 class Report1000h {
-  constructor() {
+  constructor() {}
 
+  convertMinsToHourMin(mins) {
+    const remainingMins = mins % 24;
+    const hours = (mins - remainingMins) / 60;
+    return [hours, remainingMins];
+  }
+
+  convertHourMinToMins(hours, mins) {
+    return hours * 60 + mins;
   }
 
   currentHourPerDay() {
-    const minsPerDay = (1000 * 60) / history[2023][11][0].durationToReach1000h;
-    const remainingMins = minsPerDay % 24;
-    const hours = (minsPerDay - remainingMins) / 60;
-    return [hours, remainingMins];
+    const historyData = history[2023][11];
+
+    // const minsPerDay = (1000 * 60) / history[2023][11][0].durationToReach1000h;
+    // const remainingMins = minsPerDay % 24;
+    // const hours = (minsPerDay - remainingMins) / 60;
+    // return [hours, remainingMins];
+
+    for (let i = 0; i <= historyData.length - 1; i++) {
+      const minsPerDay = (1000 * 60) / historyData[i].durationToReach1000h;
+      historyData[i].currentHourPerDay = this.convertMinsToHourMin(minsPerDay);
+    }
   }
-   
+
   currentHourPerHour() {
     const minsPerDay = (1000 * 60) / history[2023][11][0].durationToReach1000h;
     const minsPerHour = minsPerDay / 24;
@@ -25,12 +40,31 @@ class Report1000h {
   }
 
   averageHourPerDay() {
+    const historyData = history[2023][11];
 
+    for (let i = 1; i <= historyData.length - 1; i++) {
+      historyData[i].lastHourPerDay = historyData[i - 1].currentHourPerDay;
+
+      const calcualteMinsCurrentHourPerDay = this.convertHourMinToMins(
+        historyData[i].currentHourPerDay[0],
+        historyData[i].currentHourPerDay[1]
+      );
+
+      const calculateMinsLastHourPerDay = this.convertHourMinToMins(
+        historyData[i].lastHourPerDay[0],
+        historyData[i].lastHourPerDay[1]
+      );
+
+      const calculateAvgHourPerDay =
+        (calcualteMinsCurrentHourPerDay + calculateMinsLastHourPerDay) / 2;
+
+      const result = this.convertMinsToHourMin(calculateAvgHourPerDay);
+
+      historyData[i].averageHourPerDay = result;
+    }
   }
 
-  averageHourPerHour() {
-    
-  }
+  averageHourPerHour() {}
 
   // render() {
   //   html.dateOfMeasurement.textContent = `${new Date()}`;
@@ -39,8 +73,10 @@ class Report1000h {
   save() {
     //Hint: for this problem [2023][11][0] must implemented dynamically
     history[2023][11][0].dateOfMeasurement = new Date();
-    history[2023][11][0].currentHourPerDay = this.currentHourPerDay();
+    // history[2023][11][0].currentHourPerDay = this.currentHourPerDay();
     history[2023][11][0].currentHourPerHour = this.currentHourPerHour();
+    this.currentHourPerDay();
+    this.averageHourPerDay();
   }
 }
 
@@ -50,5 +86,6 @@ const report11172023 = new Report1000h();
 report11122023.save();
 report11172023.save();
 
+console.log(history);
 //This export is for testing purposes
-export { history, report11122023 , report11172023};
+// export { history, report11122023, report11172023 };
--- src/database.js
+++ src/database.js
@@ -4,28 +4,39 @@ const history = {
       {
         durationToReach1000h: 5, //5 days
         reach: 73000,
-        lastHourPerDay: "",
-        currentHourPerDay: [200, 0],
-        lastHourPerHour: "",
+        lastHourPerDay: '',
+        currentHourPerDay: '',
+        lastHourPerHour: '',
         currentHourPerHour: [8, 20],
-        averageHourPerDay: "",
-        averageHourPerHour: "",
+        averageHourPerDay: '',
+        averageHourPerHour: '',
         dateOfMeasurement:
-          "Sun Nov 19 2023 23:00:30 GMT+0330 (Iran Standard Time)",
+          'Sun Nov 19 2023 23:00:30 GMT+0330 (Iran Standard Time)'
       },
       {
         durationToReach1000h: 6,
         reach: 74000,
         lastHourPerDay: [200, 0],
-        currentHourPerDay: "",
-        lastHourPerHour: [8, 20],
-        currentHourPerHour: "",
-        averageHourPerDay: "",
-        averageHourPerHour: "",
-        dateOfMeasurement: "",
+        currentHourPerDay: '',
+        lastHourPerHour: '',
+        currentHourPerHour: '',
+        averageHourPerDay: '',
+        averageHourPerHour: '',
+        dateOfMeasurement: ''
       },
-    ],
-  },
+      {
+        durationToReach1000h: 7,
+        reach: 75000,
+        lastHourPerDay: [200, 0],
+        currentHourPerDay: '',
+        lastHourPerHour: '',
+        currentHourPerHour: '',
+        averageHourPerDay: '',
+        averageHourPerHour: '',
+        dateOfMeasurement: ''
+      }
+    ]
+  }
 };
 
 export default history;
