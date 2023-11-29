58f0cc092cdae312dc99dbbca8f30b2e5963785f
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-11-29 23:06:09 +0330

    new Record added > Wed Nov 29 2023 23:05:11 GMT+0330 (Iran Standard Time)

---
+++ database/history.js
@@ -0,0 +1,53 @@
+const History = {
+  2023: {
+    11: [
+      {
+        durationToReach1000h: 5, //5 days
+        reach: 73000,
+        lastHourPerDay: "",
+        currentHourPerDay: [200, 0],
+        lastHourPerHour: "",
+        currentHourPerHour: [8, 20],
+        averageHourPerDay: "",
+        averageHourPerHour: "",
+        dateOfMeasurement:
+          "Sun Nov 19 2023 23:00:30 GMT+0330 (Iran Standard Time)",
+      },
+      {
+        durationToReach1000h: 6,
+        reach: 74000,
+        lastHourPerDay: [200, 0],
+        currentHourPerDay: "",
+        lastHourPerHour: [8, 20],
+        currentHourPerHour: "",
+        averageHourPerDay: "",
+        averageHourPerHour: "",
+        dateOfMeasurement: "Sun Nov 17 2023 23:00:30 GMT+0330 (Iran Standard Time)",
+      },
+      {
+        durationToReach1000h: 6,
+        reach: 75000,
+        lastHourPerDay: "",
+        currentHourPerDay: "",
+        lastHourPerHour: "",
+        currentHourPerHour: "",
+        averageHourPerDay: "",
+        averageHourPerHour: "",
+        dateOfMeasurement: "Fri Nov 24 2023 23:12:53 GMT+0330 (Iran Standard Time)",
+      },
+      {
+        durationToReach1000h: 5,
+        reach: 76000,
+        lastHourPerDay: "",
+        currentHourPerDay: "",
+        lastHourPerHour: "",
+        currentHourPerHour: "",
+        averageHourPerDay: "",
+        averageHourPerHour: "",
+        dateOfMeasurement: "Wed Nov 29 2023 23:05:11 GMT+0330 (Iran Standard Time)",
+      },
+    ],
+  },
+};
+   
+export default History;
